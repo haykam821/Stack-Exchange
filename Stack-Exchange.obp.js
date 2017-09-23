9f858bcf3f01a5a9eccec7f9811531a06d732cfc
haykam821 <haykam821@users.noreply.github.com>
2017-09-23 10:09:14 -0400

    let's add a little comment

--- Stack-Exchange.obp.js
+++ Stack-Exchange.obp.js
@@ -1,7 +1,9 @@
 const request = require("request");
 const key = doc.stackExchangeKey;
 
-var actualThings = function(topic, bot, doc, user, userID, channelID, message, event) {}
+var actualThings = function(topic, bot, doc, user, userID, channelID, message, event) {
+  ///2.2/search/advanced?page=1&pagesize=1&order=desc&sort=relevance&q=INSERT TITLE HERE&accepted=True&site=stackoverflow
+}
 
 exports.onMessageReceived = (function StackExchange(bot, doc, user, userID, channelID, message, event) {
   require('./../exports.js').registerCmd(['sesearch <topic>', 'stacksearch <topic>'], 'Searches Stack Exchange for an answered question.');
