bf44154556f10a2505145d57469e395b845cc55d
haykam821 <haykam821@users.noreply.github.com>
2017-09-23 10:01:58 -0400

    Beautify time

--- Stack-Exchange.obp.js
+++ Stack-Exchange.obp.js
@@ -1,12 +1,11 @@
 const request = require("request");
 const key = doc.stackExchangeKey;
 
-var actualThings = function(topic, bot, doc, user, userID, channelID, message, event) {
-}
+var actualThings = function(topic, bot, doc, user, userID, channelID, message, event) {}
 
 exports.onMessageReceived = (function StackExchange(bot, doc, user, userID, channelID, message, event) {
   require('./../exports.js').registerCmd(['sesearch <topic>', 'stacksearch <topic>'], 'Searches Stack Exchange for an answered question.');
-  
+
   if (message === undefined) {
     return;
   }
