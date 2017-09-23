4f5582a3fb32792de6b93b10733389ad8fa0171c
haykam821 <haykam821@users.noreply.github.com>
2017-09-23 10:01:27 -0400

    Shell time

--- Stack-Exchange.obp.js
+++ Stack-Exchange.obp.js
@@ -1,1 +1,21 @@
+const request = require("request");
+const key = doc.stackExchangeKey;
 
+var actualThings = function(topic, bot, doc, user, userID, channelID, message, event) {
+}
+
+exports.onMessageReceived = (function StackExchange(bot, doc, user, userID, channelID, message, event) {
+  require('./../exports.js').registerCmd(['sesearch <topic>', 'stacksearch <topic>'], 'Searches Stack Exchange for an answered question.');
+  
+  if (message === undefined) {
+    return;
+  }
+  if (message.startsWith(doc.prefix + "sesearch ")) {
+    var topic = message.replace(doc.prefix + "sesearch ", "");
+    actualThings(topic, bot, doc, user, userID, channelID, message, event);
+  }
+  if (message.startsWith(doc.prefix + "sesearch ")) {
+    var topic = message.replace(doc.prefix + "stacksearch ", "");
+    actualThings(topic, bot, doc, user, userID, channelID, message, event);
+  }
+});
